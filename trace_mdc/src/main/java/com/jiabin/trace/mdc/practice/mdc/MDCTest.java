73ef77f2735d6eac7f51451b73110f1daba6a35e
yujiabin <yujiabin@cloudcore.cn>
2025-01-20 09:59:24 +0800

    项目新增并修改

--- trace_mdc/src/main/java/com/jiabin/trace/mdc/practice/mdc/MDCTest.java
+++ trace_mdc/src/main/java/com/jiabin/trace/mdc/practice/mdc/MDCTest.java
@@ -1,15 +1,18 @@
 package com.jiabin.trace.mdc.practice.mdc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-
 public class MDCTest {
+
+  private static ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(100));
+
 
   public static void main(String[] args) {
 
@@ -22,12 +25,14 @@ public class MDCTest {
     
     MDC.remove("traceXId") ;
     
-    try (ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(100))) {
+    try {
       Map<String, String> contextMap = MDC.getCopyOfContextMap();
       executor.execute(() -> {
         MDC.setContextMap(contextMap) ;
         logger.info("执行远程操作操作...") ;
       }) ;
+    }catch(RuntimeException e){
+      System.out.println(e.getMessage());
     }
   }
   
